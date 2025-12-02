53be79c785f5cfc8e815446f7352e1a981bf1f23
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-12-02 17:56:39 +0800

    despawn fix

--- src/main/java/me/kall/yourender/YourEnder.java
+++ src/main/java/me/kall/yourender/YourEnder.java
@@ -3,14 +3,21 @@ package me.kall.yourender;
 import com.google.common.collect.Lists;
 import me.kall.duplicationless.config.JsonConfig;
 import me.kall.duplicationless.event.BlockChangeEvent;
+import me.kall.duplicationless.event.ReloadCommandEvent;
 import me.kall.yourender.data.EndableBlocks;
 import me.kall.yourender.ext.Endable;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
 import net.minecraftforge.event.level.ChunkEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -24,22 +31,34 @@ import java.util.Set;
 public final class YourEnder {
     public static final String MOD_ID = "yourender";
 
-    private static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1")
-            .put("EnderManPickable", Lists.newArrayList(
-                    "create",
-                    "mekanism", "mekanismgenerators", "mekanismadditions",
-                    "gtceu",
-                    "immersiveengineering",
-                    "enderio", "endercore", "enderio_endergy",
-                    "industrialforegoing",
-                    "thermal_foundation", "thermal_expansion", "thermal_cultivation", "thermal_innovation", "thermal_dynamics", "thermal_locomotion",
-                    "refinedstorage"
-            ))
-            .put("DistanceForPicking", 6)
-            .initialize();
+    public static Set<String> PICKABLE;
+    public static Set<String> BLOCKS;
+    public static double DIST;
+    public static boolean DESPAWN;
 
-    public static final Set<String> PICKABLE = CONFIG.getSet("EnderManPickable", String.class);
-    public static final double DIST = CONFIG.getDouble("DistanceForPicking")  * CONFIG.getDouble("DistanceForPicking");
+    private static void loadConfig() {
+        JsonConfig config = JsonConfig.create(MOD_ID, "1")
+                .put("EnderManPickable", Lists.newArrayList("create", "mekanism", "mekanismgenerators", "mekanismadditions", "draconicevolution"))
+                .put("EnderManPickableSpecificBlocks", Lists.newArrayList())
+                .put("DistanceForPicking", 6)
+                .put("EnderManDespawnWithOurPickableBlocks", true)
+                .initialize();
+        PICKABLE = config.getSet("EnderManPickable", String.class);
+        BLOCKS = config.getSet("EnderManPickableSpecificBlocks", String.class);
+        DIST = config.getDouble("DistanceForPicking")  * config.getDouble("DistanceForPicking");
+        DESPAWN = config.getBoolean("EnderManDespawnWithOurPickableBlocks");
+    }
+
+    static {
+        loadConfig();
+    }
+
+    private static void setup() {
+        for (Map.Entry<ResourceKey<Block>, Block> entry : ForgeRegistries.BLOCKS.getEntries()) {
+            ResourceLocation id = entry.getKey().location();
+            ((Endable)entry.getValue()).yourEnder$set(PICKABLE.contains(id.getNamespace()) || BLOCKS.contains(id.toString()));
+        }
+    }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = MOD_ID)
     public static final class ForgeEvents {
@@ -65,17 +84,33 @@ public final class YourEnder {
             if (was) server.execute(() -> EndableBlocks.get(level).remove(level, chunk, block));
             if (is) server.execute(() -> EndableBlocks.get(level).add(level, chunk, block));
         }
+
+        @SubscribeEvent
+        public static void enderDespawn(MobSpawnEvent.AllowDespawn event) {
+            if (DESPAWN) {
+                Mob entity = event.getEntity();
+                if (entity instanceof EnderMan) {
+                    BlockState blockState = ((EnderMan) entity).getCarriedBlock();
+                    if (blockState == null) return;
+                    if (((Endable)blockState.getBlock()).yourEnder$get()) event.setResult(Event.Result.DENY);
+                }
+            }
+        }
+
+        @SubscribeEvent
+        public static void configReload(ReloadCommandEvent event) {
+            event.server.execute(() -> {
+                loadConfig();
+                setup();
+            });
+        }
     }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MOD_ID)
     public static final class ModEvents {
         @SubscribeEvent
         public static void commonSetup(FMLCommonSetupEvent event) {
-            event.enqueueWork(() -> {
-                for (Map.Entry<ResourceKey<Block>, Block> entry : ForgeRegistries.BLOCKS.getEntries()) {
-                    ((Endable)entry.getValue()).yourEnder$set(PICKABLE.contains(entry.getKey().location().getNamespace()));
-                }
-            });
+            event.enqueueWork(YourEnder::setup);
         }
     }
 }
